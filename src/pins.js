da225161f21bf0a06e5c60b4f5ce273fd9fd8055
Andres Arias <andres.arias12@gmail.com>
2018-05-26 20:36:21 -0600

    Pin handling desde Node.

--- src/pins.js
+++ src/pins.js
@@ -3,18 +3,61 @@ var process = require('child_process');
 var router = express.Router();
 
 
-router.post('/switch/:status', function (req, res) {
-    var sysPath = '/sys/class/gpio/gpio4/value'
-    process.exec('echo Holaaa', (err, stdout, stderr) => {
+function pinSetup(pinNum, direction, callback) {
+    var sysPath = '/sys/class/gpio/';
+    process.exec('echo ' + pinNum + ' > ' + sysPath + 'export ; echo '
+        + direction + ' > ' + sysPath + 'gpio' + pinNum + '/direction',
+        (err, stdout, stderr) => {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                console.log('Pin ' + pinNum + ' successfully exported!');
+                callback();
+            }
+        });
+}
+
+function pinUnexport(pinNum) {
+    var sysPath = '/sys/class/gpio/';
+    process.exec('echo ' + pinNum + ' > ' + sysPath + 'unexport', (err, stdout, stderr) => {
         if (err) {
-            res.status(400);
-            res.send(err)
+            console.log(err);
+            return;
+        } else {
+            console.log('Pin ' + pinNum + ' unexported!');
             return;
         }
-        res.status(200);
-        res.send(stdout);
     });
+}
 
+function pinWrite(pinNum, value, callback) {
+    var sysPath = '/sys/class/gpio/gpio';
+    process.exec('echo ' + value + ' > ' + sysPath + pinNum + '/value', (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log('Wrote ' + value + ' on pin ' + pinNum);
+            callback();
+        }
+    });
+}
+
+router.post('/switch/:status', function (req, res) {
+    var status = req.params.status;
+    pinSetup(4, 'out', function () {
+        if (status != 1 && status != 0) {
+            res.status(400);
+            res.send('Status value must be 1 or 0.');
+        } else {
+            pinWrite(4, status, function () {
+                //pinUnexport(4);
+                res.status(200);
+                res.send("Writing successful!");
+            });
+        }
+    });
 });
 
 module.exports = router;
